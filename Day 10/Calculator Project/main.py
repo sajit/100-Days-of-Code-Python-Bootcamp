1f992cb381373205bc704d1eb1b6e080d3dfe73b
sajit <sajit.kunnumkal@gmail.com>
2026-09-09 18:44:51 +0000

    Calc

--- Day 10/Calculator Project/main.py
+++ Day 10/Calculator Project/main.py
@@ -1,4 +1,22 @@
 def add(n1, n2):
     return n1 + n2
 
+def subtract(n1, n2):
+    return n1 - n2
 
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    return n1 / n2
+
+ops = {
+    "add": add,
+    "subtract": subtract,
+    "multiply": multiply,
+    "divide": divide
+}
+n1 = int(input("Enter first number: "))
+n2 = int(input("Enter second number: "))
+operation = input("Enter operation: ")
+print(ops[operation](n1, n2))
